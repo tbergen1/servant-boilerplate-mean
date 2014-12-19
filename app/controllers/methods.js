9c4d42d44a8d25806716c62225f3bb076092a25c
Austen Collins <austencollins@gmail.com>
2014-12-19 00:33:50 -0800

    Add Stripe Checkout

---
+++ app/controllers/methods.js
@@ -0,0 +1,21 @@
+// Module dependencies.
+var mongoose = require('mongoose'),
+    User = mongoose.model('User'),
+    request = require('request'),
+    config = require('../../config/config');
+
+
+var textBlast = function(req, res, next) {
+
+	// Pull in Tiny Text From Specific Servant
+	
+	// Loop through Contacts and text each one w/ Phone Number
+	
+	
+
+}
+
+
+module.exports = {
+    
+};
--- public/js/dashboard/controllers/dashboard.js
+++ public/js/dashboard/controllers/dashboard.js
@@ -1,14 +1,39 @@
 angular.module('appDashboard').controller('DashboardController', ['$rootScope', '$scope', '$timeout', '$modal', 'ServantAngularService',
 	function($rootScope, $scope, $timeout, $modal, ServantAngularService) {
 
+
+		// Initialize Stripe Checkout
+		$scope.stripeCheckout = StripeCheckout.configure({
+		    key: 'sk_test_tJRxt5eaktaLjWRtIM3E1J4c',
+		    image: '/square-image.png',
+		    token: function(token) {
+		      // Use the token to create the charge with a server-side script.
+		      // You can access the token ID with `token.id`
+		    }
+		});
+
+		// Defaults
+		$scope.view = 'loading';
+
+
 		$scope.initialize = function() {
 			ServantAngularService.getUserAndServants().then(function(response) {
-				console.log(response);
+				console.log("User and Servants:", response);
 				$rootScope.servant = response;
 			}, function(error) {
 				console.log(error);
 			});
 		};
 
+		$scope.checkout = function(amount) {
+			// Open Checkout with further options
+		    $scope.stripeCheckout.open({
+		      	name: 'Servant Text Messenger',
+		      	description: '350 Text Messages/month for $' + amount + '.00',
+		      	amount: amount,
+		      	panelLabel: 'Subscribe {{ amount }}',
+		      	amount: 2000
+		    });
+		};
 	}
 ]);
