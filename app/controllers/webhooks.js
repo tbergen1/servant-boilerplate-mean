2afc45a76684a5380a97141ed39fcf53ee6d900e
Austen Collins <austencollins@gmail.com>
2015-01-09 11:05:33 -0800

    Add sms subscribing

--- app/controllers/webhooks.js
+++ app/controllers/webhooks.js
@@ -1,11 +1,59 @@
 // Module dependencies.
 var mongoose = require('mongoose'),
     User = mongoose.model('User'),
+    ServantMeta = mongoose.model('ServantMeta'),
     TwilioHelper = require('../twilio_helper'),
     Config = require('../../config/config');
 
+// Instantiate ServantSDK
+var ServantSDK = require('servant-sdk-node')({
+    application_client_id: process.env.SERVANT_CLIENT_ID,
+    application_client_secret: process.env.SERVANT_SECRET_KEY
+});
+
+
 var twilioIncomingSMS = function(req, res, next) {
-    console.log("WEBHOOK FROM TWILIO: ", req.body, req.query, req.params)
+    console.log("WEBHOOK FROM TWILIO: ", req.body, req.query, req.params);
+
+    ServantMeta.find({
+        twilio_phone_number: req.body.To
+    }).limit(1).populate('user').exec(function(error, servantmetas) {
+        if (error) console.log("Webhook Error (Twilio) - Finding ServantMeta: ", error);
+        var servantmeta = servantmetas[0];
+        if (servantmeta) {
+            // Process Keyword
+
+
+            // Query Contacts to see if user exists
+            var criteria = {
+                query: {
+                    phone_numbers: {
+                        phone_number: {
+                            $in: [req.body.From, req.body.From.replace('+1', '')]
+                        }
+                    }
+                },
+                sort: {},
+                page: 1
+            };
+
+            ServantSDK.queryArchetypes(servantmeta.user.servant_access_token, servantmeta.servant_id, 'contact', criteria, function(error, response) {
+                if (error) console.log(error);
+                if (!response.records.length) {
+                    // Create Contact
+                    ServantSDK.saveArchetype(servantmeta.user.servant_access_token, servantmeta.servant_id, 'contact', newContact, function(error, contact) {
+                    	if (error) console.log(error);
+                    	console.log(contact);
+                    });
+                }
+            });
+        }
+    });
+
+
+    res.send({
+        message: "thanks"
+    });
 };
 
 module.exports = {
